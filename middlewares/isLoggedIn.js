9e7d0a2d6c6bda702230bd993fb7cf089f44baa3
Nitesh <niteshranjankar5@gmail.com>
2024-07-10 23:54:30 +0530

    "Updated isLoggedIn middleware to verify user existence and added error handling; updated routes/index.js to import and use the updated middleware"

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -3,19 +3,27 @@ const expressSession = require("express-session");
 const userModel = require(`../models/user-model`);
 const jwt = require("jsonwebtoken");
 
-module.exports.isLoggedIn = async (req, res) => {
+module.exports.isLoggedIn = async (req, res, next) => {
   if (!req.session.user) {
-    req.flash("error", "You are not logged in !");
+    req.flash("error", "You are not logged in!");
     return res.redirect("/");
   }
+
   try {
     let decoded = jwt.verify(req.cookies.token, process.env.JWT_KEY);
     let user = await userModel
       .findOne({ email: decoded.email })
       .select("-password");
+
+    if (!user) {
+      req.flash("error", "User not found!");
+      return res.redirect("/");
+    }
+
     req.user = user;
     next();
   } catch (error) {
-    console.log(error.message);
+    req.flash("error", error.message);
+    res.redirect("/");
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const isLoggedIn = require('../middlewares/isLoggedIn');
+const {isLoggedIn} = require('../middlewares/isLoggedIn');
+// console.log(typeof isLoggedIn);
 
 router.get("/", (req, res) => {
     let error = req.flash("error");
