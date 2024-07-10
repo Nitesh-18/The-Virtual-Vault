a00c5050c8192c15d284595b4ff0bc0cca2d4c7f
Nitesh <niteshranjankar5@gmail.com>
2024-07-10 17:03:32 +0530

    "Added bcrypt and jwt dependencies, implemented password hashing and token generation in user registration route"

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userModel = require(`../models/user-model`);
-// console.log("USER MODEL HERE:",userModel);
 
 router.get("/", (req, res) => {
   res.send("Hiu");
@@ -11,16 +12,38 @@ router.get("/", (req, res) => {
 router.post("/register", async (req, res) => {
   try {
     let { email, password, name } = req.body;
+
     let user = await userModel.findOne({ email });
-    if (user) res.status(503).send("Already registered with this email !");
-    let createdUser = await userModel.create({
-      name,
-      email,
-      password,
+    if (user) {
+      return res.status(503).send("Already registered with this email !");
+    }
+
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return res.status(400).send(err.message);
+      }
+
+      bcrypt.hash(password, salt, async function (err, hash) {
+        if (err) {
+          return res.status(400).send(err.message);
+        } else {
+          try {
+            let createdUser = await userModel.create({
+              name,
+              email,
+              password: hash,
+            });
+
+            let token = jwt.sign({ email, id: createdUser._id }, "prachi");
+            res.status(200).send({ token });
+          } catch (createErr) {
+            res.status(500).send(createErr.message);
+          }
+        }
+      });
     });
-    res.status(200).send(createdUser);
   } catch (err) {
-    res.send(err.message);
+    res.status(500).send(err.message);
   }
 });
 
