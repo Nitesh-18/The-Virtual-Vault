5f9254b0a51ff80c20e242518a6b3011d8c79fb2
Nitesh <niteshranjankar5@gmail.com>
2024-07-11 16:24:08 +0530

    "Updated products router to create product with additional fields and modified create product view to match field names"

--- routes/productsrouter.js
+++ routes/productsrouter.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const upload = require('../controllers/multer-config');
+const productModel = require('../models/product-model');
 const router = express.Router();
 
-router.post("/create",upload.single('image'), (req,res) => {
-    res.send(req.file);
+router.post("/create",upload.single('image'),async (req,res) => {
+    let {name,price,discount,textColor,panelColor,bgColor} = req.body;
+    let product = productModel.create({
+        image: req.file.buffer,
+        name ,
+        price,
+        discount,
+        bgColor,
+        panelColor,
+        textColor,
+    })
 })
 
 module.exports = router;
