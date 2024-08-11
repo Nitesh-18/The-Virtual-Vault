1a0408f38edd039838deb6fdbc72ca662faa18db
Nitesh <niteshranjankar5@gmail.com>
2024-08-11 19:22:06 +0530

    "Updated cart route and view to display dynamic product information and calculate total bill"

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { isLoggedIn } = require("../middlewares/isLoggedIn");
 const productModel = require("../models/product-model");
-const userModel  = require("../models/user-model");
+const userModel = require("../models/user-model");
 // console.log(typeof isLoggedIn);
 
 router.get("/", (req, res) => {
   let error = req.flash("error");
   let success = req.flash("success");
-  res.render("index", { error ,success, loggedin: false});
+  res.render("index", { error, success, loggedin: false });
 });
 
-router.get("/addtocart/:productid",isLoggedIn, async (req, res) => {
-  let user = await userModel.findOne({email: req.user.email});
+router.get("/addtocart/:productid", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
   user.cart.push(req.params.productid);
   await user.save();
   req.flash("success", "Product Added to Cart");
@@ -24,15 +24,20 @@ router.get("/shop", isLoggedIn, (req, res) => {
   productModel
     .find()
     .then((products) => {
-      res.render("shop", { products , success});
+      res.render("shop", { products, success });
     })
     .catch((err) => {
       console.error(err);
       res.status(500).render("error");
     });
 });
-router.get("/cart", isLoggedIn, (req, res) => {
-  res.render("cart");
+router.get("/cart", isLoggedIn, async (req, res) => {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("cart");
+
+  const bill = (Number(user.cart[0].price) + 20 ) - Number (user.cart[0].discount);
+  res.render("cart", {user,bill});
 });
 
 router.get("/logout", isLoggedIn, (req, res) => {
