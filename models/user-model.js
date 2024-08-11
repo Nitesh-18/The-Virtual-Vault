77abe84575efb4260c2f5951922a738876892b0d
Nitesh <niteshranjankar5@gmail.com>
2024-08-11 17:03:40 +0530

    "Updated user model to store cart as array of product IDs, added add to cart route, modified shop route to display success message, and updated header and shop views to reflect changes."

--- models/user-model.js
+++ models/user-model.js
@@ -4,10 +4,10 @@ const userSchema = mongoose.Schema({
     name: String,
     email: String,
     password: String,
-    cart: {
-        type: Array,
-        default: []
-    },
+    cart: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'product',
+    }],
     orders: {
         type: Array,
         default: []
--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,37 @@ const express = require("express");
 const router = express.Router();
 const { isLoggedIn } = require("../middlewares/isLoggedIn");
 const productModel = require("../models/product-model");
+const userModel  = require("../models/user-model");
 // console.log(typeof isLoggedIn);
 
 router.get("/", (req, res) => {
   let error = req.flash("error");
   let success = req.flash("success");
-  res.render("index", { error ,success});
+  res.render("index", { error ,success, loggedin: false});
+});
+
+router.get("/addtocart/:productid",isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({email: req.user.email});
+  user.cart.push(req.params.productid);
+  await user.save();
+  req.flash("success", "Product Added to Cart");
+  res.redirect("/shop");
 });
 
 router.get("/shop", isLoggedIn, (req, res) => {
+  let success = req.flash("success");
   productModel
     .find()
     .then((products) => {
-      res.render("shop", { products });
+      res.render("shop", { products , success});
     })
     .catch((err) => {
       console.error(err);
       res.status(500).render("error");
     });
+});
+router.get("/cart", isLoggedIn, (req, res) => {
+  res.render("cart");
 });
 
 router.get("/logout", isLoggedIn, (req, res) => {
